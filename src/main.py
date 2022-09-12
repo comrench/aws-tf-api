d1b1b9584bc34c9fe3d658dc9a59c906222177fc
Mahesh Kumar <maheshkumar@Maheshs-Mini.hitronhub.home>
2022-09-12 14:24:12 -0400

    Added the SQS events

--- src/main.py
+++ src/main.py
@@ -7,7 +7,9 @@ import json
 
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    print("Starting Lambda")
+    print(event)
+    # return {
+    #     'statusCode': 200,
+    #     'body': json.dumps(event)
+    # }
